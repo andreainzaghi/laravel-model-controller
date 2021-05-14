2f95f714d8404453366d7fff7cea32d0229e24fb
Andrea <andrea3aprile99@gmail.com>
2021-05-14 10:56:43 +0200

    controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $movie=Movie::all();
+        return view('welcome',['movie' ->$movies]);
     }
 }
