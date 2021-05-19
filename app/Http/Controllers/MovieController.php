dabd5e3ba329118a6189234fc45439be13dd14b9
Andrea <andrea3aprile99@gmail.com>
2021-05-19 15:13:30 +0200

    controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,13 +1,137 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Movie;
+
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    public function index(){
-        $movie=Movie::all();
-        return view('welcome',['movie' ->$movies]);
+    protected $requestValidation = [];
+
+    public function __construct()
+    {
+        $year = date("Y") + 1;
+
+        $this->requestValidation = [
+            'title' => 'required|string|max:100',
+            'film_director' => 'required|string|max:50',
+            'genres' => 'required|string|max:50',
+            'plot' => 'required|string',
+            'year' => 'required|numeric|min:1900|max:'.$year
+        ];
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return view('movies.index', ['movies' => $movies]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        if ( $data['cover_image'] === NULL ) {
+            unset($data['cover_image']);
+        }
+
+        $request->validate($this->requestValidation);
+
+        $movieNew = Movie::create($data);
+
+        // $movieNew = new Movie();
+        // $movieNew->title = $data['title'];
+        // $movieNew->film_director = $data['film_director'];
+        // $movieNew->genres = $data['genres'];
+        // $movieNew->plot = $data['plot'];
+        // $movieNew->year = $data['year'];
+
+        // if( !empty($data['cover_image']) ) {
+        //     $movieNew->cover_image = $data['cover_image'];
+        // }
+
+        // $movieNew->save();
+
+        return redirect()->route('movies.index')->with('message', 'Il film ' . $movieNew->title . ' è stato aggiunto');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        return view('movies.show', ['movie' => $movie]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie $movie)
+    {
+        return view('movies.edit', ['movie' => $movie]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        $data = $request->all();
+
+        if ( $data['cover_image'] === NULL ) {
+            unset($data['cover_image']);
+        }
+
+        $request->validate($this->requestValidation);
+
+        $movie->update( $data );
+
+        return redirect()->route('movies.show', $movie);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        $movie->delete();
+
+        return redirect()->route('movies.index')->with('message', 'Il film è stato eliminato');
     }
 }
